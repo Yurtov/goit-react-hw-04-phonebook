65a1d7c25b737758e84a859e9651d3a821a59df8
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-08-24 16:10:49 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import { AiOutlineUserAdd, AiOutlineClose } from 'react-icons/ai';
 import { ContactForm } from './ContactForm/ContactForm';
@@ -22,96 +22,78 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-    isModalOpen: false,
-  };
-
-  componentDidMount() {
-    const savedContacts = localStorage.getItem(localStorageKey);
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
-    }
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem(localStorageKey);
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
   }
+  return [];
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem(
-        localStorageKey,
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(getInitialContacts);
+  const [filter, setFilter] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  openModal = () => this.setState({ isModalOpen: true });
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contacts));
+  }, [contacts]);
 
-  closeModal = () => this.setState({ isModalOpen: false });
+  const openModal = () => setIsModalOpen(true);
 
-  addContact = newContact => {
-    this.state.contacts.some(
+  const closeModal = () => setIsModalOpen(false);
+
+  const addContact = newContact => {
+    contacts.some(
       contact =>
         contact.name.toLowerCase().trim() ===
           newContact.name.toLowerCase().trim() ||
         contact.number.trim() === newContact.number.trim()
     )
       ? alert(`${newContact.name} is already in contact`)
-      : this.setState(prevState => ({
-          contacts: [newContact, ...prevState.contacts],
-        }));
+      : setContacts(prevState => [...prevState, newContact]);
   };
 
-  handleDelete = deleteContactEl => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== deleteContactEl
-        ),
-      };
-    });
-  };
-
-  searchByFilter = newFilter => {
-    this.setState({ filter: newFilter });
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-
-    const visiblesContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+  const handleDelete = deleteContactEl => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== deleteContactEl)
     );
+  };
 
-    return (
-      <Layout>
-        <Title>Phonebook</Title>
+  const searchByFilter = newFilter => {
+    setFilter(newFilter);
+  };
 
-        <Filter onChange={this.searchByFilter} filter={filter} />
-        <Contacts>
-          <SubTitle>Contacts</SubTitle>
-          <BtnOpen onClick={this.openModal}>
-            <AiOutlineUserAdd size={45} />
-          </BtnOpen>
-        </Contacts>
-        <ContactList contacts={visiblesContacts} onClick={this.handleDelete} />
-        <Modal
-          isOpen={this.state.isModalOpen}
-          onRequestClose={this.closeModal}
+  const visiblesContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <Layout>
+      <Title>Phonebook</Title>
+
+      <Filter onChange={searchByFilter} filter={filter} />
+      <Contacts>
+        <SubTitle>Contacts</SubTitle>
+        <BtnOpen onClick={openModal}>
+          <AiOutlineUserAdd size={45} />
+        </BtnOpen>
+      </Contacts>
+      <ContactList contacts={visiblesContacts} onClick={handleDelete} />
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <BtnClose onClick={closeModal}>
+          <AiOutlineClose size={25} />
+        </BtnClose>
+        <ContactForm
+          onAddContact={addContact}
+          onClose={closeModal}
           style={customStyles}
-        >
-          <BtnClose onClick={this.closeModal}>
-            <AiOutlineClose size={25} />
-          </BtnClose>
-          <ContactForm
-            onAddContact={this.addContact}
-            onClose={this.closeModal}
-            style={customStyles}
-          />
-        </Modal>
-      </Layout>
-    );
-  }
-}
+        />
+      </Modal>
+    </Layout>
+  );
+};
